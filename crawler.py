3671975ca5252a91330eef1f45d7c4e23c10163c
TomHacker <3472661791.qq.com>
2018-06-12 20:10:26 +0800

    crawler

--- crawler.py
+++ crawler.py
@@ -1,76 +1,129 @@
 import requests
+import chardet
 from bs4 import BeautifulSoup
-import re
-import codecs
-from contextlib import closing
-import json
 import pymongo
-import gridfs
-import os
-import pymysql
-client=pymongo.MongoClient('localhost',27017)
-db=client.sense
-data=db.data
-imgput=gridfs.GridFS(db)
-
-
-download_url="https://movie.douban.com/top250"
-def download_page(url):
-    headers={
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/63.0.3239.132 Safari/537.36'
+import urllib
+import urllib.request
+import time
+import urllib.error
+import socket
+socket.setdefaulttimeout(20)  # 设置socket层的超时时间为20秒
+client=pymongo.MongoClient()
+db=client.Mydatabase
+Company=db.Company
+Person=db.Person
+def Company_crawler():
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/60.0.3112.113 Safari/537.36",
+        'Cookie': "your own cookie"
     }
-    data=requests.get(url,headers=headers).text
-    return data
-def parse_html(html):
-    sp=BeautifulSoup(html,"html.parser")
-    movielistsoup=sp.find('ol',attrs={'class':"grid_view"})
-    movie_list=[]
+    pre_url = "http://data.cfi.cn/cfidata.aspx?sortfd=&sortway=&curpage="
+    post_url = "&fr=content&ndk=A0A1934A1939A1947A1952&xztj=&mystock="
+    for i in range(1, 74):
+        url = pre_url + str(i) + post_url
+        data = requests.get(url, headers=headers).text
+        soup = BeautifulSoup(data, "html.parser")
+        table = soup.find_all("table", "table_data")
+        for i in table:
+            res = i.find_all('tr')[1:]
 
-    for movie in movielistsoup.find_all('li'):
-        detail=movie.find('div',attrs={'class':'hd'})
-        movie_name=detail.find('span',attrs={'class':'title'}).getText()
-        movie_score=movie.find('span',attrs={'class':'rating_num'}).getText()
-        movie_list.append(movie_name+movie_score)
-    next_page=sp.find('span',attrs={'class':'next'}).find('a')
-    if next_page:
-        return movie_list,download_url+next_page['href']
-    return movie_list,None
-def getpic(data):
-    pic_list=re.findall('src="http.+?.jpg"',data)
-    return pic_list
-def download_pic(url,name):
-    rootpath="S:\\pycharm\\database_system_project\\pic\\"
-    response=requests.get(url,stream=True)
-    pic_type='.'+url.split('.')[-1]
-    with closing(requests.get(url,stream=True)) as response:
-        with open(rootpath+name+pic_type,'wb') as file:
-            for data in response.iter_content(128):
-                file.write(data)
+            for ind, tmp in enumerate(res):
+                # print(ind,tmp)
+                company_info = {}
+                tds = tmp.find_all('td')
+                company_info['代码'] = tds[0].find_all('a')[0].contents[0]
+                company_info['证券简称'] = tds[1].find_all('a')[0].contents[0]
+                company_info['公司名称'] = tds[2].contents[0]
+                company_info['公司注册地址'] = tds[3].contents[0]
+                company_info['公司注册地址邮箱'] = tds[4].contents[0]
+                company_info['首次注册登陆地点'] = tds[5].contents[0]
+                company_info['企业法人营业执照注册号'] = tds[6].contents[0]
+                company_info['法人代表'] = tds[7].contents[0]
+                company_info['总经理'] = tds[8].contents[0]
+                Company.insert(company_info)
+
+def Person_scrawler():
+    headers={
+        'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                     "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+        'Cookie':"your own cookie"
+    }
+    url="http://www.cninfo.com.cn/cninfo-new/information/companylist"
+    data=requests.get(url,headers).text
+    soup=BeautifulSoup(data,'html.parser')
+    #先爬到企业名称和代码，以及详细公司链接，再根据链接爬到高管页面，再提取高管信息。
+    company_list=soup.find_all("ul","company-list")
+
+    for company in company_list:
+
+
+        li_list=company.find_all("li")
+        for li in li_list:
+            temp = li.find_all("a")[0]
+            try:
+                # print(temp["href"])
+                # 找出高管页面
+                code = temp['href'].split('?')[-1]
+                # http://www.cninfo.com.cn/information/management/szmb000001.html
+                url_new = "http://www.cninfo.com.cn/information/management/" + code + ".html"
+                # data_new=requests.get(url_new,headers)
+                uf=urllib.request.Request(url_new,headers=headers)
+
+
+
+
+                fp = urllib.request.urlopen(uf)
+                # print(fp.info())
+
+                # #gb2312
+                # print(data_new.encoding)
+
+                soup_new = BeautifulSoup(fp.read().decode('gbk'), "html.parser")
+                table_new = soup_new.find_all("div", "clear")[0]
+
+                tr_list = table_new.find_all("tr")[1:]
+                for tr in tr_list:
+                    person = {}
+                    listwords = temp.contents[0].split(' ')
+
+                    person["代码"] = listwords[0]
+
+                    person["证券简称"] = ''.join(listwords[1:])
+
+                    info = tr.find_all("td")
+                    print(info)
+                    person["姓名"] = info[0].contents[0][:-3]
+                    person["职务"] = info[1].contents[0][:-3]
+                    person["出生年月"] = info[2].contents[0][:-3]
+                    person["性别"] = info[3].contents[0][:-2]
+                    person["学历"] = info[4].contents[0][:-2]
+                    print(person)
+                    print('\n')
+
+                    Person.insert(person)
+                fp.close()
+            except urllib.error.URLError as e:
+                print(e.reason)
+            time.sleep(5)
+
+
 if __name__=='__main__':
-    url=download_url
-    n=1
-    # db=pymysql.connect('localhost','testuser','test123','TESTDB')
-    # cursor=db.cursor()
-    # cursor.execute('SELECT VERSION()')
-    # data=cursor.fetchone()
-    # print(data)
-    # db.close()
-    with codecs.open('movies','w',encoding='utf-8') as fp:
-        while(url):
-            html=download_page(url)
-            picdata=getpic(html)
-            index=0
-            movies,url=parse_html(html)
+    Person_scrawler()
+    # Company_crawler()
 
-            for picinfo in picdata:
-                download_pic(picinfo[5:-1],'Top' + str(n) +'-'+ movies[index])
-                src={'name':movies[index]}
-                data.insert_one(src)
-                print(movies[index]+'下载完毕')
-                n+=1
-                index+=1
-            fp.write(u'\n'.join(movies))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
